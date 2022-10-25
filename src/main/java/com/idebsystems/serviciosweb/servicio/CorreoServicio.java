c79770014be7ad1d793f730b3d673c7ea430be02
jorgerpm <jorgepm.4382@gmail.com>
2022-10-25 14:57:44 -0500

    modificaciones para el envio de correo, tlsv1.2

--- src/main/java/com/idebsystems/serviciosweb/servicio/CorreoServicio.java
+++ src/main/java/com/idebsystems/serviciosweb/servicio/CorreoServicio.java
@@ -12,6 +12,8 @@ import com.idebsystems.serviciosweb.entities.ArchivoXml;
 import com.idebsystems.serviciosweb.entities.Parametro;
 import com.idebsystems.serviciosweb.entities.Usuario;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
@@ -91,7 +93,7 @@ public class CorreoServicio {
 
             String host = paramHost.getValor(); //"smtp.office365.com";
             int port = Integer.parseInt(paramPuerto.getValor()); //587;
-            String userName = paramUserMail.getValor();// "jorgep_m@hotmail.com";
+            String userName = paramUserMail.getValor();
             String password = paramClaveMail.getValor();
 
             Properties properties = new Properties();
@@ -99,8 +101,11 @@ public class CorreoServicio {
             properties.put("mail.smtp.port", port);
             properties.put("mail.smtp.auth", "true");
             properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
             properties.put("mail.user", userName);
             properties.put("mail.password", password);
+            //
+            properties.setProperty("mail.debug.auth", "true");
 
             // creates a new session with an authenticator
             Authenticator auth = new Authenticator() {
@@ -112,10 +117,27 @@ public class CorreoServicio {
             Session session = Session.getInstance(properties, auth);
 
             LOGGER.info("paramNomRemit::: " + nombreRemitente);
+            
+            //seccion para comprobar si el correo tiene mas de uno, con el ;
+            List<InternetAddress> correosDestino = new ArrayList<>();
+            if(correo.contains(";")){
+                List<String> correos = Arrays.asList(correo.split(";"));
+                for(String c : correos){
+                    if(Objects.nonNull(c) && !c.isBlank()){
+                        InternetAddress addressEmail = new InternetAddress(c);
+                        correosDestino.add(addressEmail);
+                    }
+                }
+            }
+            else{
+                InternetAddress addressEmail = new InternetAddress(correo);
+                correosDestino.add(addressEmail);
+            }
+            
 
             Message msg = new MimeMessage(session);
             msg.setFrom(new InternetAddress(aliasCorreoEnvio, nombreRemitente));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(correo, ""));
+            msg.addRecipients(Message.RecipientType.TO, correosDestino.toArray(new InternetAddress[correosDestino.size()]));
             msg.setSubject(asunto);
 //            msg.setText(mensajeText);
             BodyPart messageBodyPart = new MimeBodyPart();
